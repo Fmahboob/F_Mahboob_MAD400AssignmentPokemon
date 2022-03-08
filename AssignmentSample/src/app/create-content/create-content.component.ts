40b51871936d199386a29bc47972c7c477df7321
Fmahboob <91558787+Fmahboob@users.noreply.github.com>
2022-03-07 19:35:07 -0500

    Made form for input

--- AssignmentSample/src/app/create-content/create-content.component.ts
+++ AssignmentSample/src/app/create-content/create-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Content } from '../helper-files/content-interface';
 
 @Component({
   selector: 'app-create-content',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateContentComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
+  
+
 
 }
